980cd7a8069b23568db9e979787cbac2d2e89062
Himanshu Shekhar <42701850+battleplayer02@users.noreply.github.com>
2021-02-11 11:18:19 +0530

    Update script.js

--- script.js
+++ script.js
@@ -564,7 +564,10 @@
         // provide crossword entries in an array of objects like the following example
         // Position refers to the numerical order of an entry. Each position can have
         // two entries: an across entry and a down entry
-        var puzzleData = 
+        var puzzleData;
+        $.getJSON('data.json',(data)=>{
+            puzzleData = data;
+        });
 
         $('#puzzle-wrapper').crossword(puzzleData);
 
